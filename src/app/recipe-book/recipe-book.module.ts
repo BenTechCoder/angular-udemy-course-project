68e6f683498622a59d07afbdca8fc41fa7a377b5
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2024-01-05 10:57:17 -0500

    Complete course section 3

--- src/app/recipe-book/recipe-book.module.ts
+++ src/app/recipe-book/recipe-book.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipeBookComponent } from './recipe-book.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 
 @NgModule({
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'test recipe',
+      'descriptions',
+      'https://th.bing.com/th/id/OIP.TsYH1c4KWAjcJtgLKh2i7AHaE8?w=282&h=189&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+    ),
+    new Recipe(
+      'test recipe',
+      'descriptions',
+      'https://th.bing.com/th/id/OIP.TsYH1c4KWAjcJtgLKh2i7AHaE8?w=282&h=189&c=7&r=0&o=5&dpr=1.3&pid=1.7'
+    ),
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
