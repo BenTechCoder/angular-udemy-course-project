a0b4e7dc23b0b9277a1751daf26818154db87562
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2024-01-31 14:44:00 -0500

    did section 7 of course homework

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,20 +1,26 @@
-import { Component, EventEmitter, Output, ElementRef, ViewChild} from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Output,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
-  styleUrls: ['./shopping-list-edit.component.css']
+  styleUrls: ['./shopping-list-edit.component.css'],
 })
 export class ShoppingListEditComponent {
-@Output() ingredientEditEmitter = new EventEmitter<Ingredient>()
-@ViewChild('nameInput', {static:false}) nameInputRef: ElementRef;
-@ViewChild('amountInput', {static:false}) amountInputRef: ElementRef;
-emitIngredient(name:HTMLInputElement, amount:HTMLInputElement) {
-this.ingredientEditEmitter.emit(new Ingredient(name.value, parseInt(amount.value)))
-this.amountInputRef.nativeElement.value = "";
-this.nameInputRef.nativeElement.value = "";
-}
-
-
+  @Output() ingredientEditEmitter = new EventEmitter<Ingredient>();
+  @ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
+  emitIngredient(name: HTMLInputElement, amount: HTMLInputElement) {
+    this.ingredientEditEmitter.emit(
+      new Ingredient(name.value, parseInt(amount.value))
+    );
+    this.amountInputRef.nativeElement.value = '';
+    this.nameInputRef.nativeElement.value = '';
+  }
 }
